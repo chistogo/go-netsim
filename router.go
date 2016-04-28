79cdbb01375bce2ae2451accb035be890e3a667d
Chris <christophergoulet@outlook.com>
2016-04-27 22:02:40 -0500

    Basicly Recreated Everything

--- router.go
+++ router.go
@@ -5,192 +5,320 @@ import (
     "net"
     "time"
     "log"
+    //"io"
     "io/ioutil"
     "os/exec"
     "encoding/json"
-    "github.com/twmb/algoimpl/go/graph"
     "strconv"
 	"os"
 )
 
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< GRAPH >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 
- 
- 
-//Router This struct represents a router and contains it's properties
 
-type Neighbour struct{
-    IP string
-    Port int
-    Weight int
+func (graph *Graph) Dijkstra(source string, target string)(dist map[string]int,path []string){
+    
+      //Unvisted
+      Q := make(map[string]*Node)
+      dist = make(map[string]int)
+      prev := make(map[string]*Node)
+      
+      for key , val := range graph.Nodes {             // Initialization
+          dist[key] = 9999999999                  // Unknown distance from source to v
+          prev[key] = nil                 // Previous node in optimal path from source
+          Q[key] = val                     // All nodes initially in Q (unvisited nodes)
+      }
+      
+      dist[source] = 0                        // Distance from source to source
+      
+      for(len(Q) != 0){
+          u ,keyy := min(Q,dist)    // Source node will be selected first    
+          delete(Q,keyy) //remove u from Q 
+          //for each neighbor v of u: 
+          for key, value := range u.Edges{          // where v is still in Q.
+          
+              alt := dist[keyy] + value
+              if alt < dist[key]{               // A shorter path to v has been found
+                  dist[key] = alt 
+                  prev[key] = u 
+              }
+          }
+          
+      }
+      
+      
+      /*
+Now we can read the shortest path from source to target by reverse iteration:
+1  S ← empty sequence
+2  u ← target
+3  while prev[u] is defined:                  // Construct the shortest path with a stack S
+4      insert u at the beginning of S         // Push the vertex onto the stack
+5      u ← prev[u]                            // Traverse from target to source
+6  insert u at the beginning of S             // Push the source onto the stack
+
+
+*/
+      
+        var S []string
+        u := target
+        // Construct the shortest path with a stack S
+        for(prev[u] != nil){
+            // Push the vertex onto the stack
+            S = append([]string{u},S...)
+            u = graph.getKey(prev[u])                            // Traverse from target to source
+        }
+        
+      return dist, S
+    
+    
     
 }
 
-type Router struct{
-    IP string
-    Port int
-    Neighbours []Neighbour
+//u := *Node in Q with min dist[u] 
+
+func (graph *Graph) getKey(node *Node) (key string) {
+    for key ,val := range graph.Nodes{
+        if(val == node){
+            return key
+        }
+    }
+    
+    return "ERROR"
+}
+
+func min(Q map[string]*Node,dist map[string]int) (*Node,string) {
+    
+    var min int
+    var minNode *Node
+    firstLoop := true
+    var keyy string
+    
+    for key ,value := range Q{
+        if(firstLoop){
+            min = dist[key]
+            minNode = value
+            firstLoop = false
+            keyy = key
+        }else{
+            if(dist[key]< min){
+                min = dist[key]
+                minNode = value    
+                keyy = key   
+            }
+        }
+    }
+    
+    
+    return minNode ,keyy
 }
 
 
-
-//Vertices
-type Host struct{
-    Connections map[string]int
+type Node struct{
+    Edges map[string]int
 }
 
-//Graph
-type NetworkGraph struct{
-    Hosts map[string]*Host
+type Graph struct{
+    Name string
+    TotalNodes int
+    Nodes map[string]*Node
 }
 
 
-
-
-func (host *Host) addConnection(ip string, weight int){
-    host.Connections[ip]=weight
+func (graph *Graph) addUndirectedWeightedVertice(idOfNode1 string ,idOfNode2 string ,weight int){
+    
+    graph.Nodes[idOfNode1].Edges[idOfNode2] = weight
+    graph.Nodes[idOfNode2].Edges[idOfNode1] = weight
+    
 }
 
-func (graph *NetworkGraph) addHost(newHost string){
-    myHost := Host{
+func createGraph(graphName string) *Graph {
+    graphThis := Graph{ 
+        Name : graphName,
+        TotalNodes : 0,
+    }
+    
+    graphThis.Nodes = make(map[string]*Node)
+    
+    return &graphThis
+}
+
+func (graph *Graph) addNode(idOfNode string) {
+    
+    //TODO: Add Real error
+    if _ , isKey := graph.Nodes[idOfNode]; isKey{
+        fmt.Println("Key already exists. Did not add node")
+    }else{
+        
+    
+        graph.TotalNodes = graph.TotalNodes + 1
+        newNode := Node{} 
+
+        graph.Nodes[idOfNode] = &newNode
+
+        newNode.Edges = make(map[string]int)
+            
         
     }
     
-    graph.Hosts[newHost] = &myHost
-    graph.Hosts[newHost].Connections = make(map[string]int)
 }
 
-func createGraph() *NetworkGraph  {
-    myGraph := NetworkGraph{
+func (graph *Graph) toJson()([]byte){
+    theJSON, _ := json.Marshal(graph)
+    return theJSON
+}
+
+
+func (graph *Graph) removeNode(idOfNode string){
+    
+ 
+   for key, val := range graph.Nodes{
        
-    }
-    myGraph.Hosts = make(map[string]*Host)
-    return &myGraph
+       for keyy := range val.Edges{
+           if keyy == idOfNode {
+               delete(graph.Nodes[key].Edges,keyy)
+           }
+       }
+       
+       
+       if(key == idOfNode){
+           delete(graph.Nodes,key)
+           graph.TotalNodes = graph.TotalNodes -1
+       }
+   }
+    
 }
 
-func (graph *NetworkGraph) getHost(host string)*Host{
-   
-   return graph.Hosts[host]
-   
-  
+
+
+
+
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<END OF GRAPH >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+func createRouter(ip string) *Router{
+    this := Router{}
+    this.IP = ip
+    this.Neighbours = make(map[string]int)
+    return &this
+}
+ 
+ 
+type Router struct{
+    IP string
+    Neighbours map[string]int
+}
+
+//Change IP of router
+func (router *Router) setIP(ip string){
+    router.IP = ip
+}
+
+//Add Neighbours
+func (router *Router) addNeighbour(ip string, weight int){
+    router.Neighbours[ip] = weight
+}
+//Add Neighbours
+func (router *Router) removeNeighbour(ip string){
+    delete(router.Neighbours,ip)
 }
 
 
 
-func listenForRouter(router Router) {
-    println("listening")
-    for true {
-        In, err := net.Listen("tcp", ":" + strconv.Itoa(router.Port))
-        checkError(err)
-        for {
-            _, err := In.Accept()
-            if err != nil {
-                checkError(err)
-            }
-            println("hi")
-                    
-                    
-        }
-    }
-}
 
 
 func main(){
    
-   myGraph := createGraph();
-   
-   myGraph.addHost("192.168.1.1:8088")
-   myGraph.addHost("192.168.1.2:8088")
-   
-   host := myGraph.getHost("192.168.1.2:8088")
-   
-   host.addConnection("192",2)
-   host.addConnection("194",33)
-  
-   
-   
-
    clear()
    println("ʕ◔ϖ◔ʔ  Welcome to the GO NetSim, Router Process!!!  ʕ◔ϖ◔ʔ")
    
    
-   fileName := "routerinfo.json"
-   if(len(os.Args[1:]) == 1){
-       fileName = os.Args[1]
-   }
-
-   
-   
+    fileName := "router1.json"
+    if(len(os.Args[1:]) == 1){
+        fileName = os.Args[1]
+    }
    
    //Read in JSON file to create the router Struct
-   data, err := ioutil.ReadFile(fileName)
-   checkError(err)
+    data, err := ioutil.ReadFile(fileName)
+    checkError(err)
    
    //Create Router Struct and turn json into Struct
-   var testRouter Router
-   err = json.Unmarshal(data,&testRouter)
-   checkError(err)
+    var router *Router
+    err = json.Unmarshal(data,&router)
+    checkError(err)
+ 
+    //Create Router
+    // router := createRouter("127.0.0.1:9001")
+    // router.addNeighbour("127.0.0.1:9002",0)
+    // router.addNeighbour("127.0.0.1:9003",0)
     
-    //Create a Go Routine that listens for other routers that are trying to comunicate
-    go listenForRouter(testRouter)
+    timeStamp , _ := time.Now().MarshalText()
+    
+    graph := createGraph(string(timeStamp))
+    theJSON, _ := json.Marshal(router)
+    //theJSON, _ = json.Marshal(graph)
+    
+    scanForNeighbours(router,graph)
+    
+    fmt.Println(string(theJSON))
+    
+    go listenForScan(router)
+    
+    for{
+        time.Sleep(time.Second * 15)
+        scanForNeighbours(router,graph)
+    }
 
-    //Turn our Router to json , this was for testing
-    //b, _ := json.Marshal(testRouter);
-    // s := string(b)
     
-    //fmt.Println(s)
-    
-    //Create Graph undirected weighted graph to represent the current network
-    tree := graph.New(graph.Undirected)
-    nodes := make(map[string]graph.Node, 0)
-    nodeWeIs := testRouter.IP + ":" + strconv.Itoa(testRouter.Port)
-    nodes[nodeWeIs] = tree.MakeNode()
-    
-    //Interate through the nodes in the router(the one read from json) and add them to the graph
-    for i := 0; i < len(testRouter.Neighbours); i++ {
-        
-        currentProcessingNode := testRouter.Neighbours[i].IP + ":" + strconv.Itoa(testRouter.Neighbours[i].Port)
-        nodes[currentProcessingNode] = tree.MakeNode()
-        tree.MakeEdgeWeight(nodes[nodeWeIs], nodes[currentProcessingNode], testRouter.Neighbours[i].Weight)
-        
-    }
-    //Set values of all nodes to key???????? Clarification needed.
-    for key, node := range nodes {
-        *node.Value = key
-    }
-    
-    //Test to find the minimum spanning tree.
-    //mst := tree.DijkstraSearch(nodes[nodeWeIs])
-    
-    //Turn that tree to json for sending
-    b, _ := json.Marshal(myGraph);
-    s := string(b)
-    //Print out the json for testing
-    fmt.Println(s)
-    
-    
-    //This function trys to initiates connection to the other routers and updates the tree if they are connected
-    boot(testRouter)
-    
-    //inifinate loop to keep the program running while the go routines do their thing
-    for true {
-        
-    }
-    
-    }
- func boot(myRouter Router) {
-     // for node in neighbors
-     //    are you alive
-     for i:=0; i < len(myRouter.Neighbours); i++ {
-         fmt.Println(myRouter.Neighbours[i].IP)
-         _, err:= net.DialTimeout("tcp", myRouter.Neighbours[i].IP + ":" + strconv.Itoa(myRouter.Neighbours[i].Port), time.Duration(1) * time.Second)
-         //checkError(err)
-         if(err != nil && err.(net.Error).Timeout()) {
-             println("ʕ◔ϖ◔ʔ halp, we timed the fuck out ʕ◔ϖ◔ʔ")
-         }
-     }
-        
  }
+
+func listenForScan(router *Router)  {
+    ln, err := net.Listen("tcp",router.IP)
+    checkError(err)
+    for{
+        conn, err := ln.Accept()
+        if err != nil {
+            checkError(err)
+        }
+        go handleListenForScan(conn,router)
+    }
+}
+
+func handleListenForScan(conn net.Conn,router *Router)  {
+    //Generate Random Weight
+}
+
+
+func scanForNeighbours(router *Router,graph *Graph){
+    
+    for ip, _ := range router.Neighbours{
+        conn, err := net.Dial("tcp", ip)
+      
+        //Error Checking
+         
+        
+        if(checkNetworkError(err)){
+            timeStamp , _ := time.Now().MarshalText()
+            fmt.Fprintf(conn, router.IP)
+            bytes , err := ioutil.ReadAll(conn)
+            if err != nil {
+                checkError(err)
+            }   
+            //Update Weight in router
+            router.Neighbours[ip], err = strconv.Atoi(string(bytes))   
+            if err != nil {
+                checkError(err)
+            }
+            //Add To Graph
+            graph.addNode(ip)
+            graph.addUndirectedWeightedVertice(router.IP,ip,router.Neighbours[ip])  
+            //Update TimeStamp
+            graph.Name = string(timeStamp)
+            
+        }
+        
+    }
+    
+}
 
 
 
@@ -210,6 +338,22 @@ func checkError(err error)  {
     if err != nil {
         log.Fatal(err)
     }
+}
+
+func checkNetworkError(err error) (shouldRun bool) {
+  skip := false
+  if err == nil {
+    skip = true
+  } else if netErr, ok := err.(net.Error); ok && netErr.Timeout() {
+    println("Timeout")
+  } else if opError, ok := err.(*net.OpError); ok {
+    if opError.Op == "dial" {
+      println("Unknown host")
+    } else if opError.Op == "read" {
+      println("Connection refused")
+    }
+  }
+  return skip
 }
 
 
